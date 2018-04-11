65332fe3e54314a431723e8973b4c17484756b78
Michael LeDuc <michael.leduc@hmhco.com>
2018-04-11 08:46:35 -0400

    changes

--- samee-mi-automation/src/test/java/samee_mi_automation/samee_mi/Stepdefs.java
+++ samee-mi-automation/src/test/java/samee_mi_automation/samee_mi/Stepdefs.java
@@ -8,8 +8,7 @@ import cucumber.api.java.en.When;
 public class Stepdefs {
     @Given("^I have (\\d+) cukes in my belly$")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
-        Belly belly = new Belly();
-        belly.eat(cukes);
+    	System.out.println(System.getProperty("math.inventory.test.browser"));
     }
     @When("^I wait (\\d+) hour$")
     public void i_wait_hour(int arg1) throws Throwable {
